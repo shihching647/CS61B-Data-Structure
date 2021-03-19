2deabba5c85b24790d957632c9671d2cc3bb31de
scliou <ariel647@hotmail.com>
2021-03-20 02:37:21 +0800

    [lab12] Sorting

--- src/laboratory/lab12/YourSort.java
+++ src/laboratory/lab12/YourSort.java
@@ -4,5 +4,11 @@ public class YourSort {
 
   public static void sort(int[] A) {
     // Place your Part III code here.
+    int n = A.length;
+    if (n <= 60) {
+      Sort.insertionSort(A);
+    } else {
+      Sort.quicksort(A);
+    }
   }
 }
